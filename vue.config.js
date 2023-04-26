a3540126e0f8991a007320a4573b3ca308a4d8c1
xjh <15200180653@163.com>
2023-04-26 18:30:59 +0800

    修改线上配置

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,8 @@ console.info(process.env.NODE_ENV,"====");
 console.info(process.env.VUE_APP_ENV_PATH,"====");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: process.env.NODE_ENV === 'development' ?{
-    host: "localhost",
-    port: `8089`,
-    open: true,
-    // 代理链接配置
-    proxy: {
-      [process.env.VUE_APP_ENV_PATH]: {
-        target: process.env.VUE_APP_ENV_PATH,
-        changeOrigin: true,
-        pathRewrite: {
-          ["^" + process.env.VUE_APP_ENV_PATH]: process.env.VUE_APP_ENV_PATH
-        },
-        logLevel: 'debug',
-        secure: true,
-      }
-    }
-  } :{
-    publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  devServer: {
     host: "localhost",
     port: `8089`,
     open: true,
